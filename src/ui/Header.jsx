392d0909620a3079b86bc75edd4cd3e8847f15cf
Negar Parvaresh <negar.prv@gmail.com>
2023-08-12 10:02:25 +0330

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <Link to="/">Fast React Pizza Co.</Link>
+
+        <p>Negar</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
